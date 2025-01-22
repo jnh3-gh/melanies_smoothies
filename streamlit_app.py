759d25821bf68b4b51d143569b0051e1a7f36dae
jnh3-gh <87740429+jnh3-gh@users.noreply.github.com>
2025-01-22 00:00:26 -0500

    Update streamlit_app.py
    Using SEARCH_ON value

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,12 +38,12 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen, ' is ',  search_on, '.')
+        # st.write('The search value for ', fruit_chosen, ' is ',  search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
         # smoothiefruit site not working for me. Revert to fruityvice.
         # smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-        fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/" + fruit_chosen)
+        fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/" + search_on)
         fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width = True)
 
     # st.write(ingredients_string)
